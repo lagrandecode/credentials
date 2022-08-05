eeacfc19e68302bf7e6d9f67b2a90dd42a31e430
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-05 22:38:36 +0100

    "commit"

--- credentials/urls.py
+++ credentials/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('registration.urls'))
+    path('register/', include('registration.urls')),
+    path('', include('mainscreen.urls'))
 ]
--- registration/views.py
+++ registration/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def registerview(request):
+    return render(request, 'register.html')
