e8d9ca30ab5aa8959fed54f47f74b3f9afab2f66
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-07 08:30:15 +0100

    "commit"

--- registration/urls.py
+++ registration/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.registerview, name='register')
+    path('', views.registerview, name='register'),
+    path('login', views.loginview, name='login')
 ]
