8ae600520ada8fba59a09e3aa8ec71a467cd5e68
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-10 23:44:10 +0100

    "commit"

--- credentials/settings.py
+++ credentials/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIR=[BASE_DIR / 'static']
-STATIC_ROOT = 'static/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
