5cd3a6663f58c17f75c5f7ca83fc1965a272b0bf
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-11 00:11:07 +0100

    "commit"

--- credentials/settings.py
+++ credentials/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR=[BASE_DIR / 'static']
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/'
+STATICFILES_DIR= [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
