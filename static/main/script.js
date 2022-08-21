618427b085f811c9deaa381cb280848231e25853
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-21 19:20:05 +0100

    "commit"

--- static/main/script.js
+++ static/main/script.js
@@ -1,9 +1,9 @@
 
 
 let menu = document.querySelector('#bars');
-let nav  = document.querySelector('.navbar');
+let navbar  = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
-    nav.classList.toggle('open'); 
+    navbar.classList.toggle('active'); 
 }
