667806d0e7a379930d99e9d585d0c0c4faa8127f
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-25 19:46:56 +0100

    "commit"

--- static/main/script.js
+++ static/main/script.js
@@ -9,19 +9,16 @@ x.onclick = () => {
 }
 
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".home-container", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 7500,
       disableOnInteraction: false,
     },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    loop:true,
   });
