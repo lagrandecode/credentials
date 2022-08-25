9a24701020ba0e76319b516b4de2d8101e3bb297
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-25 19:44:07 +0100

    "addding swipe animation"

--- static/main/script.js
+++ static/main/script.js
@@ -7,3 +7,21 @@ x.onclick = () => {
     x.classList.toggle('fa-times');
     y.classList.toggle('open'); 
 }
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
