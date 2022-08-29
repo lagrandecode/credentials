28cef541860dfb8775eda530d04d4635cc4ab1dd
lagrandecode <seunogunmolufirst1@gmail.com>
2022-08-29 19:23:59 +0100

    "commit"

--- credentials/settings.py
+++ credentials/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
     'registration.apps.RegistrationConfig',
     'mainscreen.apps.MainscreenConfig'
@@ -51,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+    
 ]
 
 ROOT_URLCONF = 'credentials.urls'
